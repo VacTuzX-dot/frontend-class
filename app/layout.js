03db68f8ab45aaa88e79abdf3bb3d386ae991d76
VacTuzX-dot <lilitxrie@gmail.com>
2025-07-02 11:31:03 +0700

    font

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font";
 import "./globals.css";
 import Navbar from "./components/nav";
 import Footer from "./components/footer";
