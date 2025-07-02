057cd0e9fb31d581b87fb7c4daa4645463fa8af6
VacTuzX-dot <lilitxrie@gmail.com>
2025-07-02 12:23:16 +0700

    uncomments

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
-// import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/nav";
 import Footer from "./components/footer";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "013 Taweesak Numma",
